02ab541316e41c1458ccbccb9dbae6346b71cabb
Haroen Viaene <hello@haroen.me>
2016-02-11 14:23:05 +0100

    k

--- _includes/k.js
+++ _includes/k.js
@@ -2,7 +2,7 @@ var links = {% include k.json %}
 
 var k = function() {
   var k = Math.floor(Math.random() * (links.length));
-  document.getElementById('k').innerHTML = '<source src="'+links[k]+'" type="video/mp4">';
+  document.getElementById('k').innerHTML = '<video onclick="k();" poster="k.png" autoplay loop muted><source src="'+links[k]+'" type="video/mp4"></video>';
   document.getElementById('link').href = links[k];
   console.log('k');
 };
